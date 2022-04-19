dd2fc1b30030846b0ad68b742d75e9425a0293e6
Lawal Hayde <81729609+Haydelawal@users.noreply.github.com>
2022-04-19 13:41:51 +0100

    Add files via upload

---
+++ ayodeji/admin log in/dist/loginadmin.php
@@ -0,0 +1,89 @@
+<?php
+
+include 'connect.php';
+
+if(!empty($_POST['submit'])) {
+
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $query = "SELECT * FROM creds WHERE email='$email' AND password='$password'";
+
+  $result = mysqli_query($con,$query);
+  $count = mysqli_num_rows($result);
+
+  if($count > 0){
+    echo "<script>alert('LOGIN SUCCESS');</script>";
+    header("Location: http://localhost/ayodeji/admin/index.php");
+
+  }else {
+    ECHO  "<script>alert('LOG IN NOT SUCCESSFUL')</script>";
+
+  }
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login Page</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"> <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-beta/css/bootstrap.min.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="container">
+        <div class="row">
+            <div class="col-lg-3 col-md-2"></div>
+            <div class="col-lg-6 col-md-8 login-box">
+                <div class="col-lg-12 login-key">
+                    <i class="fa fa-key" aria-hidden="true"></i>
+                </div>
+                <div class="col-lg-12 login-title">
+                    ADMIN LOGIN
+                </div>
+
+                <div class="col-lg-12 login-form">
+                    <div class="col-lg-12 login-form">
+                    <form method="post">
+                            <div class="form-group">
+                                <label class="form-control-label">EMAIL ADDRESS</label>
+                                <input type="text" name="email" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label class="form-control-label">PASSWORD</label>
+                                <input type="password" name="password" class="form-control" i>
+                            </div>
+
+                            <div class="col-lg-12 loginbttm">
+                                <div class="col-lg-6 login-btm login-text">
+                                    <!-- Error Message -->
+                                </div>
+                                <div class="col-lg-6 login-btm login-button">
+                                    <input class="btn btn-outline-primary" type="submit" name="submit" value="LOG IN" />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-2"></div>
+            </div>
+        </div>
+<!-- partial -->
+  
+</body>
+</html>
---
+++ ayodeji/list/list.php
@@ -0,0 +1,83 @@
+<?php
+
+include 'connect.php';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Reports</title>
+	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+	<link rel="stylesheet" href="styless.css">
+</head>
+<body>
+
+
+<div class="wrapper">
+	<div class="links">
+		<ul>
+			<li data-view="list-view" class="li-list active">
+			<i class="fas fa-file"></i>
+			List Of Security Reports 
+		</li>
+		</ul>
+	</div>
+
+
+	
+
+	<div class="view_main">
+		<div class="view_wrap list-view" style="display: block;">
+		
+
+		<?php
+         $sql = "select * from `report`";
+         $result = mysqli_query($con, $sql);
+         if($result){
+
+
+          while($row = mysqli_fetch_assoc($result)){
+
+			$id=$row['id'];
+
+			$title =$row['title'];
+			$main =$row['main'];
+
+			$writer =$row['writer'];
+
+
+            $date =$row['date'];
+
+			echo'
+		
+		<div class="view_item">
+				<div class="vi_left">
+					<img src="tomato.png" alt="tomato">
+				</div>
+			
+
+				<div class="vi_right">
+					<p class="title">'.$title.'</p>
+					<p class="content">'.$date.'</p>
+					<p class="content">'.$writer.'</p>
+					<div class="btn"><a href="index.php?indexid='.$id.'">Read More</a></div>
+				</div>
+			</div>';
+
+		  }
+		}
+			?>
+			
+		</div>
+		
+	</div>
+</div>
+	
+<script src="scriptss.js"></script>
+
+</body>
+</html>
